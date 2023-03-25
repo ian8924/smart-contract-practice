314099846f25be3d08c05c227dc199daa4c54f9b
ian <justforian0915@gmail.com>
2023-03-25 22:10:19 +0800

    fix

--- contracts/stateVariavle.sol
+++ contracts/stateVariavle.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 contract StateVariables {
     uint public myUnit = 123;
 
-    function foo() public {
+    function foo() public pure {
         uint notStateVariable = 456;
     }
 }
