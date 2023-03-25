f3201c1c4d10fad3c1c7326161a8ba39d2bca1e4
ian <justforian0915@gmail.com>
2023-03-25 22:35:58 +0800

    update : Constants

---
+++ contracts/Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+//378 gas
+contract Constants {
+  address public constant MY_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+ uint public constant MY_UINT = 123;
+}
+// 2489 gas
+contract Vars {
+    address public MY_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+}
